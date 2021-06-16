5c23877df8c739fd7fe7b532dff6383cac251062
claudio-code <suporte2424@outlook.com>
2021-06-15 23:32:57 -0300

    added examples to custom configs fields in Models entities

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -15,4 +15,43 @@ class Post extends Model
         'body',
         'published_at',
     ];
+
+    /**
+     * used if your class name doesn't match database table name.
+     *
+     * @var string
+     */
+    protected $table = 'posts';
+
+    /**
+     * used if primary key is not named id.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * if your class don't use timestamps change value to 'false'.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+
+    /**
+     * if your classe use other connection set it in variable connection
+     *
+     * @var string
+     */
+    protected $connection = 'mysql';
+
+    /**
+     * if your class uses custom fields, define it name in array.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'published_at',
+    ];
 }
